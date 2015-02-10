09b15e59c3246e7b45c87a02e5e565d9862226c8
Jdkmotorhead <jdkmotorhead@gmail.com>
2015-02-09 18:16:30 -0800

    Added top and bottom limit switches to the elevator.

--- src/org/usfirst/frc/team5102/robot/Elevator.java
+++ src/org/usfirst/frc/team5102/robot/Elevator.java
@@ -1,20 +1,53 @@
 package org.usfirst.frc.team5102.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TalonSRX;
 
 public class Elevator
 {
 	private TalonSRX leftElevatorMotor, rightElevatorMotor;
 	
-	Elevator()
+	private Claw claw;
+	
+	private DigitalInput topElevatorLimit;
+	private DigitalInput bottomElevatorLimit;
+	
+	public Elevator()
 	{
 		leftElevatorMotor = new TalonSRX(8);
 		rightElevatorMotor = new TalonSRX(9);
+		claw = new Claw();
+		topElevatorLimit = new DigitalInput(1); //check port
+		bottomElevatorLimit = new DigitalInput(2); //check port
 	}
 	
 	public void raiseElevator(double raiseAmount)
 	{
-		leftElevatorMotor.set(raiseAmount);
-		rightElevatorMotor.set(raiseAmount);
+		if(topElevatorLimit.get())
+		{
+			if(raiseAmount < 0)
+			{
+				leftElevatorMotor.set(raiseAmount);
+				rightElevatorMotor.set(raiseAmount);
+			}
+		}
+		
+		else if(bottomElevatorLimit.get())
+		{
+			if(raiseAmount > 0)
+			{
+				leftElevatorMotor.set(raiseAmount);
+				rightElevatorMotor.set(raiseAmount);
+			}
+		}
+		
+		else
+		{
+			leftElevatorMotor.set(raiseAmount);
+			rightElevatorMotor.set(raiseAmount);
+		}
+		
 	}
+	
+	//what about lower limit?
 }
