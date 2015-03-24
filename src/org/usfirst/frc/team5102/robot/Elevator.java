e8c3d0bcb655965d5aa166f1bb596bc2bc93fc49
Jdkmotorhead <jdkmotorhead@gmail.com>
2015-03-23 20:05:16 -0700

    Re-implemented top and bottom limit switches, and added "transport height" position.

--- src/org/usfirst/frc/team5102/robot/Elevator.java
+++ src/org/usfirst/frc/team5102/robot/Elevator.java
@@ -3,17 +3,14 @@ package org.usfirst.frc.team5102.robot;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Talon;
 
 public class Elevator extends RobotElement
 {
 	private Talon elevatorMotor;
 	
-	private DigitalInput topElevatorLimit;
-	private DigitalInput bottomElevatorLimit;
+	private DigitalInput bottomEmergencyLimit,topEmergencyLimit,bottomElevatorLimit,topElevatorLimit,transportHeightSensor,toteHeightSensor;
 	private AnalogPotentiometer potentiometer;
-	private DigitalInput toteHeightSensor;
 	private boolean userInput = true;
 	private boolean practiceMode = false;
 	int counter = 0;
@@ -30,7 +27,8 @@ public class Elevator extends RobotElement
 	{
 		top,
 		bottom,
-		tote
+		tote,
+		transport
 	}
 	
 	public Elevator()
@@ -41,47 +39,102 @@ public class Elevator extends RobotElement
     	intake = new Intake();
     	
 		elevatorMotor = new Talon(6);
-		topElevatorLimit = new DigitalInput(0); //check port
-		bottomElevatorLimit = new DigitalInput(1); //check port
-		toteHeightSensor = new DigitalInput(2);
+		
+		topEmergencyLimit = new DigitalInput(0);
+		bottomEmergencyLimit = new DigitalInput(1);
+		topElevatorLimit = new DigitalInput(2);
+		bottomElevatorLimit = new DigitalInput(3);
+		transportHeightSensor = new DigitalInput(4);
+		toteHeightSensor = new DigitalInput(5);
+		
 		potentiometer = new AnalogPotentiometer(0);
+		
 		intakeTimer = new Timer();
 		elevatorMoveTimer = new Timer();
 	}
 	
 	public boolean raiseElevator(double raiseAmount)
 	{
-		boolean topLimitStatus = topElevatorLimit.get();
-		boolean bottomLimitStatus = bottomElevatorLimit.get();
-		
+		if(topElevatorLimit.get() || topEmergencyLimit.get())
+		{
+			if(raiseAmount < 0.0)
+			{
+				elevatorMotor.set(raiseAmount);
+				return true;
+			}
+			else
+			{
+				elevatorMotor.set(0.0);
+				return true;
+			}
+		}
+		if(bottomElevatorLimit.get() || bottomEmergencyLimit.get())
+		{
+			if(raiseAmount > 0.0)
+			{
+				elevatorMotor.set(raiseAmount);
+				return true;
+			}
+			else
+			{
+				elevatorMotor.set(0.0);
+				return true;
+			}
+		}
 		elevatorMotor.set(raiseAmount);
 		return false;
-		
 	}
 	
 	public void gotoHeight(position height)
 	{
 		if(height == position.bottom)
 		{
-			raiseElevator(elevatorLowerSpeed);
-			
-			while(elevatorMoveTimer.get() < timeToLowerElevator){}
-			
-			elevatorMoveTimer.reset();
+			boolean sensor = false;
+			while(sensor == false)
+			{
+				sensor = raiseElevator(-1.0);
+			}
 		}
 			
 		else if(height == position.tote)
 		{
-			raiseElevator(-elevatorLowerSpeed);
-			
-			while(elevatorMoveTimer.get() < timeToLowerElevator){}
-			
-			elevatorMoveTimer.reset();
+			while(!toteHeightSensor.get())
+			{
+				if(potentiometer.get() < 0.5)
+				{
+					raiseElevator(1.0);
+				}
+				else if(potentiometer.get() > 0.5)
+				{
+					raiseElevator(-1.0);
+				}
+			}
+			raiseElevator(0.0);
 		}
 		
 		else if(height == position.top)
 		{
-			
+			boolean sensor = false;
+			while(sensor == false)
+			{
+				sensor = raiseElevator(1.0);
+			}
+		}
+		
+		else if(height == position.transport)
+		{
+			while(!transportHeightSensor.get())
+			{
+				if(potentiometer.get() < 0.1)
+				{
+					raiseElevator(1.0);
+				}
+				else if(potentiometer.get() > 0.1)
+				{
+					raiseElevator(-1.0);
+				}
+			}
+			raiseElevator(0.0);
 		}
 	}
 	
@@ -94,13 +147,13 @@ public class Elevator extends RobotElement
 		intakeTimer.reset();
 		
 		intake.openIntake();
-		claw.openClaw();
 		
 		gotoHeight(position.bottom);			//move the elevator to the bottom
 	
 		intake.openIntake();			//open the intake arms
 	
 		gotoHeight(position.tote);			//go to default position
+		
 		userInput = true;
 	}
 	
@@ -110,11 +163,6 @@ public class Elevator extends RobotElement
 		if(userInput)
 		{
 			raiseElevator(controller.applyDeadband(-controller.getLeftStickY()));
-		}
-		
-		if(bottomElevatorLimit.get())
-		{
-			System.out.println("bottom limit triggered");
 		}
 		
 		if(controller.getButtonA())
